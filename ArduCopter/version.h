1484a72ed0b3c64195d4a9ecedf9a25c5ccb7bbd
NickWilsonMA <nick.wilson@hover-bike.com>
2023-02-27 13:53:15 +0000

    Release of MA-Copter-4.1.1.1 with location scrubber.

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.1.1"
+#define THISFIRMWARE "MA V4.1.1.1 LS"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,1,FIRMWARE_VERSION_TYPE_OFFICIAL
