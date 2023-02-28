aebfd964ab5d5417a21a24210401dbf34ba07f5e
NickWilsonMA <nick.wilson@hover-bike.com>
2023-02-28 13:23:48 +0000

    Version to 4.1.1.2

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "MA V4.1.1.2 LS"
+#define THISFIRMWARE "MA V4.1.1.2"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,1,FIRMWARE_VERSION_TYPE_OFFICIAL
