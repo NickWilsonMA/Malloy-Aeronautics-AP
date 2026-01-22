95bdd9069c05823bd59b193f31226d8804d0f35d
Kush Makkapati <kush.makkapati@malloyaeronautics.com>
2026-01-22 17:26:49 +0000

    Allow Dijkstra to path plan when breaching a geofence

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "MA_COPTER-V4.3.0.12"
+#define THISFIRMWARE "MA_COPTER-V4.3.0.13_DEV.1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4, 3, 0, FIRMWARE_VERSION_TYPE_OFFICIAL
