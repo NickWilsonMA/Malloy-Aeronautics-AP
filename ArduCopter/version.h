fb1784069e25f6991a0d718deb1e30fa5a9a971d
BenJooMA <Ben.Joo@malloyaeronautics.com>
2026-07-14 16:31:25 +0100

    FMSMS-325: Updated version string and added binaries for release 4.3.0.16
    Part of [#FMSMS-325](https://baesystems-cloud.atlassian.net/browse/FMSMS-325)

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "MA-Copter-4.3.0.16-rc.3"
+#define THISFIRMWARE "MA-Copter-4.3.0.16"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4, 3, 0, FIRMWARE_VERSION_TYPE_OFFICIAL
