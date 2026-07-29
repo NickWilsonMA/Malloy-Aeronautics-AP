46cae65e8a6bf23cdca7f6a52604b699e89accfd
BenJooMA <Ben.Joo@malloyaeronautics.com>
2026-07-29 14:16:02 +0100

    FMSMS-387: Updated version string for Release Candidate #2
    Part of [#FMSMS-387](https://baesystems-cloud.atlassian.net/browse/FMSMS-387)

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "MA-Copter-4.3.0.17-rc.1"
+#define THISFIRMWARE "MA-Copter-4.3.0.17-rc.2"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4, 3, 0, FIRMWARE_VERSION_TYPE_OFFICIAL
